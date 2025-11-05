7fd85b747f792e96a0bc86f6a6272106da534cd3
WegenerSteven <wegenersteven580@gmail.com>
2025-11-05 23:29:06 +0300

    initial commit

---
+++ NewApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello;
+
+public class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello this is C#, welcome");
+    }
+}
