5768b6695e086667cbc4a5290d8252e422b4a1a2
WegenerSteven <wegenersteven580@gmail.com>
2025-12-26 15:47:34 +0300

    Refactor time display logic and streamline AdjustTimes method

--- testproject/Program.cs
+++ testproject/Program.cs
@@ -20,38 +20,13 @@
 
 int[] times = { 200, 800, 1600, 2000, 100, 5000 };
 
-int diff = 0;
+int diff = 0; 
 
 Console.WriteLine("Enter current GMT");
 int currentGMT = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Current Medicine Schedule:");
-
-/*Format and display medicine times */
-
-foreach (int val in times)
-{
-    string time = val.ToString();
-    int len = time.Length;
-
-    if (len >= 3)
-    {
-        time = time.Insert(len - 2, ":");
-    }
-    else if (len == 2)
-    {
-        time = time.Insert(0, "0:");
-    }
-    else
-    {
-        time = time.Insert(0, "0:0");
-    }
-
-    Console.Write($"{time} ");
-}
-
-Console.WriteLine();
-
+DisplayTimes();
 
 Console.WriteLine("Enter new GMT");
 int newGMT = Convert.ToInt32(Console.ReadLine());
@@ -60,49 +35,52 @@ if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
 {
     Console.WriteLine("Invalid GMT");
 }
-else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0) 
+
+else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
 {
     diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-
-    /* Adjust the times by adding the difference, keeping the value within 24 hours */
-    for (int i = 0; i < times.Length; i++) 
-    {
-        times[i] = ((times[i] + diff)) % 2400;
-    }
-} 
-else 
+    AdjustTimes();
+}
+else
 {
     diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-
-    /* Adjust the times by adding the difference, keeping the value within 24 hours */
-    for (int i = 0; i < times.Length; i++) 
-    {
-        times[i] = ((times[i] + diff)) % 2400;
-    }
+    AdjustTimes();
 }
 
 Console.WriteLine("New Medicine Schedule:");
+DisplayTimes();
 
-/* Format and display medicine times */
-foreach (int val in times)
+void DisplayTimes()
 {
-    string time = val.ToString();
-    int len = time.Length;
+    /*Format and display medicine times*/
+    foreach (int val in times)
+    {
+        string time = val.ToString();
+        int len = time.Length;
 
-    if (len >= 3)
-    {
-        time = time.Insert(len - 2, ":");
-    }
-    else if (len == 2)
-    {
-        time = time.Insert(0, "0:");
-    }
-    else
-    {
-        time = time.Insert(0, "0:0");
-    }
+        if (len >= 3)
+        {
+            time = time.Insert(len - 2, ":");
+        }
+        else if (len == 2)
+        {
+            time = time.Insert(0, "0:");
+        }
+        else
+        {
+            time = time.Insert(0, "0:0");
+        }
 
-    Console.Write($"{time} ");
+        Console.Write($"{time}");
+    }
+    Console.WriteLine();
 }
 
-Console.WriteLine();
+void AdjustTimes()
+{
+    //Adjust the times by adding the difference keeping the value within 24 hrs
+    for(int i = 0; i<times.Length; i++)
+    {
+        times[i] = ((times[i] + diff)) % 2400;
+    }
+}
