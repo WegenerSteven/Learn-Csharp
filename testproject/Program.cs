a57b7a1bb6e099bd3ecfcaf9665139a11c148866
WegenerSteven <wegenersteven580@gmail.com>
2025-12-27 10:32:00 +0300

    Refactor fortune teller and animal sound demonstration code; comment out unused methods and classes

--- testproject/Program.cs
+++ testproject/Program.cs
@@ -177,23 +177,191 @@ else ipAddress is invalid
 //-----------4-------------------
 /* Code challenge */
 
-Random random = new Random();
-int luck = random.Next(100);
+// Random random = new Random();
+// int luck = random.Next(100);
 
-string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
-string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
-string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
-string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
+// string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
+// string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
+// string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
+// string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
 
-Console.WriteLine("A fortune teller whispers the following words:");
-FortuneTeller();
+// Console.WriteLine("A fortune teller whispers the following words:");
+// FortuneTeller();
 
-void FortuneTeller()
+// void FortuneTeller()
+// {
+//     string[] fortune = luck > 75 ? good : (luck < 25 ? bad : neutral);
+//     for (int i = 0; i < 4; i++)
+//     {
+//         Console.Write($"{text[i]} {fortune[i]}");
+//     }
+//     return;
+// }
+
+/*-------------5------------
+class inheritance and polymorphism
+*/
+
+// HousePet mypet1 = new Dog();
+// HousePet mypet2 = new Cat();
+
+// Console.WriteLine(mypet1.Speak());
+// Console.WriteLine(mypet2.Speak());
+
+
+// public class HousePet
+// {
+//     public virtual string Speak()
+//     {
+//         return "Hello";
+//     }
+// }
+
+// public class Dog : HousePet
+// {
+//     public override string Speak()
+//     {
+//         return "woof";
+//     }
+// }
+
+// public class Cat : HousePet
+// {
+//     public override string Speak()
+//     {
+//         return "Meow";
+//     }
+// }
+
+//transitive nature of inheritance in C#
+
+// public class Vehicle
+// {
+//     public string Make { get; set; }
+//     public string Model { get; set; }
+
+//     public void StartEngine()
+//     {
+//         Console.WriteLine("Engine started.");
+//     }
+
+//     public void StopEngine()
+//     {
+//         Console.WriteLine("Engine stopped");
+//     }
+// }
+
+// public class Car : Vehicle
+// {
+//     public int NumberOfDoors { get; set; }
+
+//     public void OpenTrunk()
+//     {
+//         Console.WriteLine("Trunk opened");
+//     }
+
+//     public void HonkHorn()
+//     {
+//         Console.WriteLine("Horn honked");
+//     }
+
+//     public void LockDoors()
+//     {
+//         Console.WriteLine("Doors locked");
+//     }
+// }
+
+// public class ElectricCar : Car
+// {
+//     public int BatteryCapacity { get; set; }
+
+//     public void chargeBattery()
+//     {
+//         Console.WriteLine("Battery charging");
+//     }
+
+//     public void DisplayBatterystatus()
+//     {
+//         Console.WriteLine("Battery status displayed");
+//     }
+// }
+
+// public class CombustionEngineCar : Car
+// {
+//     public int FuelCapacity { get; set; }
+
+//     public void Refuel()
+//     {
+//         Console.WriteLine("Car Refueled.");
+//     }
+
+//     public void CheckOilLevel()
+//     {
+//         Console.WriteLine("Oil level checked");
+//     }
+// }
+
+//----------------Examine the use of the abstract keyword-----------------
+
+// public abstract class Shape //base abstract class
+// {
+//     public abstract int GetArea(); //abstract method
+// }
+
+// public class Square : Shape
+// {
+//     private int _side;
+
+//     public Square(int side)
+//     {
+//         _side = side;
+//     }
+
+//     public override int GetArea()
+//     {
+//         return _side * _side;
+//     }
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Square square = new Square(5);
+//         Console.WriteLine($"Area of the square = {square.GetArea()}");
+//     }
+// }
+/**-----------Examine the use of the virtual keyword----------*/
+
+public class Animal
 {
-    string[] fortune = luck > 75 ? good : (luck < 25 ? bad : neutral);
-    for (int i = 0; i < 4; i++)
+    public virtual void MakeSound()
     {
-        Console.Write($"{text[i]} {fortune[i]}");
+        Console.WriteLine("Animal makes a sound");
     }
-    return;
+}
+
+public class Dog: Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+public class Dog: Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal animal = new Dog();
+        animal.MakeSound();
+    }
 }
