a9d7846e8c2ae0e3bbd8948400dab6bbf9120f77
WegenerSteven <wegenersteven580@gmail.com>
2026-01-02 00:11:23 +0300

    Add initial implementation of Program class with IPerson examples

--- ImplementInterfaces/Program.cs
+++ ImplementInterfaces/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ImplementInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPerson student = new Student {Name = "Eric Solomon", Age = 20};
+            IPerson teacher = new Teacher {Name="Kayla Lewis", Age = 35};
+
+            student.DisplayInfo();
+            teacher.DisplayInfo();
+        }
+    }
+}
