b19e744c34b6959bc37f6384b033958653ad4f25
WegenerSteven <wegenersteven580@gmail.com>
2026-01-02 00:11:31 +0300

    Add IPerson interface definition with properties and method

---
+++ ImplementInterfaces/IPerson.cs
@@ -0,0 +1,10 @@
+namespace ImplementInterfaces
+{
+    //define IPerson interface
+    public interface IPerson
+    {
+        string Name {get; set;} //properties
+        int Age {get; set;} //properties
+        void DisplayInfo(); //method
+    }
+}
