a708e641196e0f2e93c9f695d247765ed98b5e17
WegenerSteven <wegenersteven580@gmail.com>
2026-01-09 20:15:56 +0300

    Add initial Program class to create BankCustomer objects

---
+++ Reuse_M1/Starter/Program.cs
@@ -0,0 +1,17 @@
+﻿using Reuse_M1;
+using System.Globalization;
+
+class Program
+{
+    static void main()
+    {
+        //create BankCustomer objects
+        string firstName = "Tim";
+        string lastName = "Shao";
+
+        Console.WriteLine($"Creating a BankCustomer object for customer {firstName} {lastName}...");
+        BankCustomer customer1 = new BankCustomer(firstName, lastName);
+
+        
+    }
+}
