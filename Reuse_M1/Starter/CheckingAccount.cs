5bf3adfae44e53336ecf1fc821b4d92990cf2ee8
WegenerSteven <wegenersteven580@gmail.com>
2026-01-12 01:09:06 +0300

    Add default values for overdraft limit and interest rate in CheckingAccount class

--- Reuse_M1/Starter/CheckingAccount.cs
+++ Reuse_M1/Starter/CheckingAccount.cs
@@ -2,12 +2,27 @@ using System;
 
 namespace Reuse_M1;
 
-public class CheckingAccount: BankAccount
+public class CheckingAccount : BankAccount
 {
-    //constructor to access base class constructor
-    public CheckingAccount(string customerId, double initialBalance) : base(customerId, initialBalance,"Checking")
+
+    //public static properties with private setters for default overdraft limit and default interest rate
+    public static double DefaultOverdraftLimit { get; private set; }
+    public static double DefaultInterestRate { get; private set; }
+
+    //public property for overdraft limit
+    public double OverdraftLimit { get; set; }
+
+    static CheckingAccount()
     {
-        
+        DefaultOverdraftLimit = 500;
+        DefaultInterestRate = 0.0;
+    }
+
+    //constructor to access base class constructor
+    public CheckingAccount(string customerIdNumber, double balance = 200, double overdraftLimit = 500) : base(customerIdNumber, balance, "Checking")
+    {
+        OverdraftLimit = overdraftLimit;
+
     }
 
 }
